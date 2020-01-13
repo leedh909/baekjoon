dc9f1e5a693585dac6f69ce41588b707d76d4db9
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-01-13 16:44:16 +0900

    Revert "Revert "2020.1.6~1.10""
    This reverts commit 7464cd87014a3827a1923bbbee5698691a234d41.

---
+++ Q2557.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+public class Q2557 {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+
+	}
+
+}
+//백준 알고리즘 제출방법
+// 클래스 main으로 해야함.
+
+//import java.util.*;
+//public class Main{
+//	public static void main(String args[]){
+//		Scanner sc = new Scanner(System.in);
+//		int a, b;
+//		a = sc.nextInt();
+//		b = sc.nextInt();
+//		System.out.println(a + b);
+//	}
+//}
