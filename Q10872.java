c18198c24f984847b878d9e2343a7876fb931a89
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-04-23 08:39:26 +0900

    2020.4.23

---
+++ Q10872.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10872 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.println(pac(num));
+		
+		
+	}
+	
+	public static int pac(int num) {
+		
+		int num2=num;
+		
+		
+		if(num==1 || num==0) {
+			num2=1;
+			return num2;
+		}else {
+			num--;
+			return num2=num2 * pac(num);
+		}
+		
+		
+		
+	}
+
+}
