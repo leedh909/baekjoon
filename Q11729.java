c51323e011c6f3251d3d4c06a833532164e5abdc
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-05-06 08:55:57 +0900

    2020.5.6

---
+++ Q11729.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q11729 {
+	//하노이 탑 이동 순서
+	
+	static int n,cnt=0;
+	static StringBuilder sb = new StringBuilder();
+	static void hanoi(int n,int from,int by,int to) {
+		cnt++;
+		if(n==1) { // 원판이 1개일 때
+			sb.append(from+" "+to+"\n");
+			return;
+		}else { // 원판이 1개가 아닐 때
+			hanoi(n-1,from,to,by); // n-1을 한 후, 1->3->2로 원판을 전달
+			sb.append(from+" "+to+"\n");
+			hanoi(n-1,by,from,to); // n-1을 한 후, 2->1->3으로 원판을 전달
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		hanoi(n,1,2,3);
+		System.out.println(cnt);
+		System.out.println(sb);
+		
+		
+		
+
+	}
+	
+	
+
+}
