2fa6a2e01641af61340999941905c92462b9af1b
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-06-10 15:49:13 +0900

    2020.6.10

--- Q1181.java
+++ Q1181.java
@@ -1,42 +1,45 @@
 package baekjoon;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.HashSet;
 
 public class Q1181 {
 	//단어 정렬
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        // 중복 제거를 위해 set으로 먼저 input
+        HashSet<String> set = new HashSet<String>();
+ 
+        for(int i=0; i<N; i++) 
+            set.add(br.readLine());
+        
+        // List 변환
+        ArrayList<String> list = new ArrayList<String>(set);
+ 
+        // Comparator 클래스를 통하여 custom 정렬
+        // 길이에 따라서 먼저 정렬하고 길이가 같으면 사전순으로 정렬
+        Collections.sort(list, new Comparator<String>() {
+            public int compare(String v1, String v2) {
+                if(v1.length() > v2.length()) 
+                    return 1;
+                else if(v1.length() < v2.length()) 
+                    return -1;
+                else
+                    return v1.compareTo(v2);
+            }
+        });
+ 
+        for(String s : list) 
+            System.out.println(s);
+    }
 		
-		String arr[][] = new String[num][2];
-		
-		for(int i=0;i<num;i++) {
-			arr[i][0]=sc.next().toLowerCase();
-			arr[i][1]=String.valueOf(arr[i][0].length());
-		}
-		
-		Arrays.sort(arr, new Comparator<String[]>() {
-
-			@Override
-			public int compare(String[] o1, String[] o2) {
-				
-				 if(o1[1]==o2[1]) {
-					 return o1[0].compareTo(o2[0]);
-				 }
-				 return o1[1].compareTo(o2[1]);
-		
-			}
-		});
-		
-		for(int i=0;i<arr.length;i++) {
-			
-			System.out.println(arr[i][0]);
-	
-		}
-		
-	}
 
 }
